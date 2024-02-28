69fd2c2f83d7e9e5fd072d9072eb97751e57f6a9
Charlotte Stone <charlie.stone649@gmail.com>
2024-02-28 16:36:21 +0000

    Update SECRET_KEY in settings

--- TeknoEspero/settings.py
+++ TeknoEspero/settings.py
@@ -23,7 +23,7 @@ if os.path.exists("env.py"):
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fyj1hn+$!e(7lwu0fg^t*$9!dg$8n9mvp)g46ps3*!(^q!)tp-"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
