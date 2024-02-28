81164230e18c66c638ca848c787ef2183207db4f
Charlotte Stone <charlie.stone649@gmail.com>
2024-02-28 16:49:46 +0000

    Update djrichtextfield config in settings

--- TeknoEspero/settings.py
+++ TeknoEspero/settings.py
@@ -60,6 +60,21 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+SITE_ID = 1
+
+DJRICHTEXTFIELD_CONFIG = {
+    'js': ['//cdn.ckeditor.com/4.14.0/standard/ckeditor.js'],
+    'init_template': 'djrichtextfield/init/ckeditor.js',
+    'settings': {
+        'toolbar': [
+            ['Format', 'Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList'], ['Undo', 'Redo'],
+            ['Maximize']
+        ],
+        'format_tags': 'p;h1;h2;h3'
+    }
+}
+
 ROOT_URLCONF = "TeknoEspero.urls"
 
 TEMPLATES = [
