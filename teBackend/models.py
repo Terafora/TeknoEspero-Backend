c9c2e8e2341be64d9b5e38b9bf7af9e34663118e
Charlotte Stone <charlie.stone649@gmail.com>
2024-02-28 15:31:48 +0000

    Create base of Blog model

--- teBackend/models.py
+++ teBackend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Blog Model
+
+class Blog(models.Model):
+    """A blog item which contains the content for posts"""
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    date = models.DateField()
+    content = models.TextField()
+    image = models.ImageField(upload_to="images/")
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+    
